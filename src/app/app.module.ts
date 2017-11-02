f692462a71aee2b8873b21fb170c8a05d598b570
HaiderMalik <haidermalik504@gmail.com>
2017-11-02 21:56:24 +0500

    added navbar and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { CompanyListComponent } from './companies/company-list/company-list.component';
+import {AppComponent} from './app.component';
+import {CompanyListComponent} from './companies/company-list/company-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {CompanyService} from "./companies/company.service";
+import {RouterModule, Routes} from "@angular/router";
+import { NavbarComponent } from './navbar/navbar.component';
+
+const routes: Routes = [{
+  path: 'companies',
+  component: CompanyListComponent
+}];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompanyListComponent
+    CompanyListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     CompanyService
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
